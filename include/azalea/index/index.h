64a05831033d9367293fc21ed829098047c0c38a
BenjaminXiang <xiangl3@mail.sustech.edu.cn>
2024-03-05 21:02:49 +0800

    Add index and searcher

--- include/azalea/index/index.h
+++ include/azalea/index/index.h
@@ -6,24 +6,75 @@
 
 namespace azalea {
 
-// using id_t = uint64_t;
-template <typename IDType, typename DistType>
+/**
+ * @brief Specify more comprehensive search parameters
+ * 
+ */
+struct SearchParameters {
+
+};
+
+/**
+ * @brief Abstract structure for an index, supports building index for vectors.
+ * 
+ * @tparam IDType The data type for storing IDs is determined by the number of vectors 
+           that need to be indexed, with the default type being int64_t.
+ * @tparam DataType The data type for vector data, with the default being float.
+ */
+template < typename DataType = float, typename IDType = int64_t>
 struct Index {
-  int dim_;
-  IDType num_;
+  int dim_;                ///< vector dimension
+  IDType num_;             ///< The total number of indexed vectors
+  MetricType metric_type_; ///< The type of metric this index uses for index building
 
-  MetricType metric_type_;
-
-  Index(int dim, IDType num, MetricType metric_type)
-      : dim_(dim),
-        num_(num),
-        metric_type_(metric_type) {}
+  /**
+   * @brief Construct a new Index object
+   * 
+   * @param dim Dimension of vectors.
+   * @param num Number of vectors to build index.
+   * @param metric_type The type of metric this index uses for index building
+   */
+  explicit
+  Index(int dim, IDType num, MetricType metric)
+    : dim_(dim),
+      num_(num),
+      metric_type_(metric) {}
 
   virtual ~Index();
 
-  virtual void add(IDType n, const DistType* vec) = 0;
+  /**
+   * @brief Build index for vectors.
+   * 
+   * @param vec_num  Number of vectors.
+   * @param kVecData Pointer to the vectors.
+   */
+  virtual void BuildIndex(IDType vec_num, const DataType* kVecData) = 0;
 
-  virtual void train(IDType n, const DistType* x) = 0;
+  /**
+   * @brief Build index for vectors with ids.
+   * 
+   * @param vec_num  Number of vectors.
+   * @param kVecIds  Pointer to the ids of vectors.
+   * @param kVecData Pointer to the vectors.
+   */
+  virtual void BuildIndexWithIds(IDType vec_num, const IDType* kVecIds, const DataType* kVecData);
+
+  /**
+   * @brief Save the index to a file on disk.
+   * 
+   * @param kFilePath File path.
+   */
+  virtual void Save(const char* kFilePath) const = 0;
+
+  /**
+   * @brief Load the index from a file.
+   * 
+   * @param kFilePath File path.
+   */
+  virtual void Load(const char* kFilePath) = 0;
+
+
+  // virtual void add(IDType n, const DistType* vec) = 0;
 
   // DistType operator() (int data_id) const {
   //   return 0;
