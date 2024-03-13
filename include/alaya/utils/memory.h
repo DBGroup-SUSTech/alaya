d9bde83e9a9733cfefb20cd3b68574003337d659
BenjaminXiang <xiangl3@mail.sustech.edu.cn>
2024-03-13 19:19:42 +0800

    Change Name to google style

--- include/alaya/utils/memory.h
+++ include/alaya/utils/memory.h
@@ -3,6 +3,9 @@
 #include <cstddef>
 #include <cstdlib>
 #include <cstring>
+
+#include "platform_macros.h"
+
 #if defined(__SSE2__)
 #include <immintrin.h>
 #elif defined(__aarch64__)
@@ -11,9 +14,8 @@
 
 namespace alaya {
 
-#define ALWAYS_INLINE __attribute__((always_inline))
-
-ALWAYS_INLINE inline void prefetch_L1(const void* address) {
+ALWAYS_INLINE
+inline void PrefetchL1(const void* address) {
 #if defined(__SSE2__)
   _mm_prefetch((const char*)address, _MM_HINT_T0);
 #else
@@ -21,7 +23,8 @@ ALWAYS_INLINE inline void prefetch_L1(const void* address) {
 #endif
 }
 
-ALWAYS_INLINE inline void prefetch_L2(const void* address) {
+ALWAYS_INLINE
+inline void PrefetchL2(const void* address) {
 #if defined(__SSE2__)
   _mm_prefetch((const char*)address, _MM_HINT_T1);
 #else
@@ -29,7 +32,8 @@ ALWAYS_INLINE inline void prefetch_L2(const void* address) {
 #endif
 }
 
-ALWAYS_INLINE inline void prefetch_L3(const void* address) {
+ALWAYS_INLINE
+inline void PrefetchL3(const void* address) {
 #if defined(__SSE2__)
   _mm_prefetch((const char*)address, _MM_HINT_T2);
 #else
@@ -37,120 +41,117 @@ ALWAYS_INLINE inline void prefetch_L3(const void* address) {
 #endif
 }
 
-inline void mem_prefetch(char* ptr, const int num_lines) {
+ALWAYS_INLINE
+inline void MemPrefetch(char* ptr, const int num_lines) {
   switch (num_lines) {
     default:
       [[fallthrough]];
     case 28:
-      prefetch_L1(ptr);
+      PrefetchL1(ptr);
       ptr += 64;
       [[fallthrough]];
     case 27:
-      prefetch_L1(ptr);
+      PrefetchL1(ptr);
       ptr += 64;
       [[fallthrough]];
     case 26:
-      prefetch_L1(ptr);
+      PrefetchL1(ptr);
       ptr += 64;
       [[fallthrough]];
     case 25:
-      prefetch_L1(ptr);
+      PrefetchL1(ptr);
       ptr += 64;
       [[fallthrough]];
     case 24:
-      prefetch_L1(ptr);
+      PrefetchL1(ptr);
       ptr += 64;
       [[fallthrough]];
     case 23:
-      prefetch_L1(ptr);
+      PrefetchL1(ptr);
       ptr += 64;
       [[fallthrough]];
     case 22:
-      prefetch_L1(ptr);
+      PrefetchL1(ptr);
       ptr += 64;
       [[fallthrough]];
     case 21:
-      prefetch_L1(ptr);
+      PrefetchL1(ptr);
       ptr += 64;
       [[fallthrough]];
     case 20:
-      prefetch_L1(ptr);
+      PrefetchL1(ptr);
       ptr += 64;
       [[fallthrough]];
     case 19:
-      prefetch_L1(ptr);
+      PrefetchL1(ptr);
       ptr += 64;
       [[fallthrough]];
     case 18:
-      prefetch_L1(ptr);
+      PrefetchL1(ptr);
       ptr += 64;
       [[fallthrough]];
     case 17:
-      prefetch_L1(ptr);
+      PrefetchL1(ptr);
       ptr += 64;
       [[fallthrough]];
     case 16:
-      prefetch_L1(ptr);
+      PrefetchL1(ptr);
       ptr += 64;
       [[fallthrough]];
     case 15:
-      prefetch_L1(ptr);
+      PrefetchL1(ptr);
       ptr += 64;
       [[fallthrough]];
     case 14:
-      prefetch_L1(ptr);
+      PrefetchL1(ptr);
       ptr += 64;
       [[fallthrough]];
     case 13:
-      prefetch_L1(ptr);
+      PrefetchL1(ptr);
       ptr += 64;
       [[fallthrough]];
     case 12:
-      prefetch_L1(ptr);
+      PrefetchL1(ptr);
       ptr += 64;
       [[fallthrough]];
     case 11:
-      prefetch_L1(ptr);
+      PrefetchL1(ptr);
       ptr += 64;
       [[fallthrough]];
     case 10:
-      prefetch_L1(ptr);
+      PrefetchL1(ptr);
       ptr += 64;
       [[fallthrough]];
     case 9:
-      prefetch_L1(ptr);
-      ptr += 64;
-      [[fallthrough]];
-    case 8:
-      prefetch_L1(ptr);
+      PrefetchL1(ptr);
       ptr += 64;
       [[fallthrough]];
     case 7:
-      prefetch_L1(ptr);
+      PrefetchL1(ptr);
       ptr += 64;
       [[fallthrough]];
     case 6:
-      prefetch_L1(ptr);
+      PrefetchL1(ptr);
       ptr += 64;
       [[fallthrough]];
     case 5:
-      prefetch_L1(ptr);
+      PrefetchL1(ptr);
       ptr += 64;
       [[fallthrough]];
     case 4:
-      prefetch_L1(ptr);
+      PrefetchL1(ptr);
       ptr += 64;
       [[fallthrough]];
     case 3:
-      prefetch_L1(ptr);
+      PrefetchL1(ptr);
       ptr += 64;
       [[fallthrough]];
     case 2:
-      prefetch_L1(ptr);
+      PrefetchL1(ptr);
       ptr += 64;
       [[fallthrough]];
     case 1:
-      prefetch_L1(ptr);
+      PrefetchL1(ptr);
       ptr += 64;
       [[fallthrough]];
     case 0:
@@ -167,6 +168,7 @@ inline void mem_prefetch(char* ptr, const int num_lines) {
  * @param num_bytes The size of the memory block to allocate, in bytes.
  * @return A pointer to the allocated memory block, or nullptr if the allocation fails.
  */
+ALWAYS_INLINE
 inline void* Alloc2M(std::size_t num_bytes) {
   std::size_t len = (num_bytes + (1 << 21) - 1) >> 21 << 21;
   auto ptr = std::aligned_alloc(1 << 21, len);
@@ -180,6 +182,7 @@ inline void* Alloc2M(std::size_t num_bytes) {
  * @param num_bytes The number of bytes to allocate.
  * @return A pointer to the allocated memory.
  */
+ALWAYS_INLINE
 inline void* Alloc64B(std::size_t num_bytes) {
   std::size_t len = (num_bytes + (1 << 6) - 1) >> 6 << 6;
   auto ptr = std::aligned_alloc(64, len);
