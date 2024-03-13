c11da1b21e96bc56d15d7795a65b6550f6b9f250
BenjaminXiang <xiangl3@mail.sustech.edu.cn>
2024-03-13 19:17:22 +0800

    Opt type_utils

--- include/alaya/utils/type_utils.h
+++ include/alaya/utils/type_utils.h
@@ -1,43 +1,70 @@
 #pragma once
 
+#include <concepts>
+#include <cstdint>
 #include <type_traits>
 #include <utility>
-#include <concepts>
 
 #include "alaya_assert.h"
 
 namespace alaya {
 
+/**
+ * @brief Returns a dependent type based on the value of template parameter N.
+ * If N is less than or equal to 8, returns uint8_t.
+ * If N is less than or equal to 16, returns uint16_t.
+ * If N is less than or equal to 32, returns uint32_t.
+ * Otherwise, returns uint64_t.
+ *
+ * @tparam N The value used to determine the return type.
+ * @return The dependent type based on the value of N.
+ */
 template <unsigned N>
-auto dependent_func() {
-    if constexpr (N <= 8) {
-        return std::declval<uint8_t>();
-    } else if constexpr (N <= 16) {
-        return std::declval<uint16_t>();
-    } else if constexpr (N <= 32) {
-        return std::declval<uint32_t>();
-    } else {
-        return std::declval<uint64_t>();
-    }
+auto DependentFunc() {
+  if constexpr (N <= 8) {
+    // return std::declval<uint8_t>();
+    return static_cast<uint8_t>(0);
+  } else if constexpr (N <= 16) {
+    // return std::declval<uint16_t>();
+    return static_cast<uint16_t>(0);
+  } else if constexpr (N <= 32) {
+    // return std::declval<uint32_t>();
+    return static_cast<uint32_t>(0);
+  } else {
+    // return std::declval<uint64_t>();
+    return static_cast<uint64_t>(0);
+  }
 }
 
+/**
+ * @brief A template struct that provides a dependent type based on the result of a dependent
+ * function.
+ *
+ * @tparam N The value used to invoke the dependent function.
+ */
 template <unsigned N>
-struct dependent_type {
-    using type = decltype(dependent_func<N>());
+struct DependentType {
+  using type = decltype(DependentFunc<N>());
 };
 
+/**
+ * @brief A type alias template that represents the type of dependent bits.
+ *
+ * This template alias is used to define the type of dependent bits based on the value of N.
+ * It relies on the `DependentType` template to determine the type.
+ *
+ * @tparam N The number of dependent bits.
+ */
 template <unsigned N>
-using dependent_type_t = typename dependent_type<N>::type;
+using DependentBitsType = typename DependentType<N>::type;
 
-constexpr unsigned GetMaxIntegral(unsigned BitsNum) {
-  return (1 << BitsNum) - 1;
-}
+/**
+ * @brief Calculates the maximum value that can be represented by an unsigned integral type with a
+ * given number of bits.
+ *
+ * @param BitsNum The number of bits in the unsigned integral type.
+ * @return The maximum value that can be represented by the unsigned integral type.
+ */
+constexpr unsigned GetMaxIntegral(unsigned BitsNum) { return (1 << BitsNum); }
 
-// template <unsigned N>
-// struct A {
-//     using T = dependent_type_t<N>;
-// };
-
-
-
-}// namespace alaya
+}  // namespace alaya
