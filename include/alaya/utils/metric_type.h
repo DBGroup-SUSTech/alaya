4e10bedb28dc6fbc93c196f30e61710112e48552
BenjaminXiang <xiangl3@mail.sustech.edu.cn>
2024-03-13 19:16:33 +0800

    Add constexpr MetricMap

--- include/alaya/utils/metric_type.h
+++ include/alaya/utils/metric_type.h
@@ -1,8 +1,11 @@
 #pragma once
 
+#include <array>
+#include <string>
+#include <string_view>
+#include <tuple>
 
 namespace alaya {
-
 
 enum class MetricType {
   L2,
@@ -10,16 +13,40 @@ enum class MetricType {
   COS,
 };
 
-// template<MetricType metric>
-// constexpr auto select_distance_func() {
-//   if constexpr (metric == MetricType::L2) {
-//     return l2_dist;
-//   } else if constexpr (metric == MetricType::IP) {
-//     return ip_dist;
-//   } else if constexpr (metric == MetricType::COS) {
-//     return cos_dist;
-//   }
-// }
+struct MetricMap {
+  static constexpr std::array<std::tuple<std::string_view, MetricType>, 3> kStaticMap = {
+      std::make_tuple("L2", MetricType::L2),
+      std::make_tuple("IP", MetricType::IP),
+      std::make_tuple("COS", MetricType::COS),
+  };
 
+  // static consteval MetricType operator[](std::string_view str) {
+  //   for (const auto& [key, val] : kStaticMap) {
+  //     if (key == str) {
+  //       return val;
+  //     }
+  //   }
+  //   __builtin_unreachable();
+  // }
 
-} // namespace alaya
+  static constexpr MetricType operator[](std::string_view str) {
+    for (const auto& [key, val] : kStaticMap) {
+      if (key == str) {
+        return val;
+      }
+    }
+    __builtin_unreachable();
+  }
+};
+
+inline constexpr MetricMap kMetricMap{};
+
+// static_assert(kMetricMap["L2"] == MetricType::L2);
+
+// inline constexpr static std::unordered_map<std::string, MetricType> kMetricMap = {
+//     {"L2", MetricType::L2},
+//     {"IP", MetricType::IP},
+//     {"COS", MetricType::COS},
+// };
+
+}  // namespace alaya
