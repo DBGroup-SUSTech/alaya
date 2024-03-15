30d2b8b6d25c750509b4cbf9bcee3d5ad985077d
BenjaminXiang <xiangl3@mail.sustech.edu.cn>
2024-03-15 18:21:31 +0800

    Add kMeans impl

--- include/alaya/utils/kmeans.h
+++ include/alaya/utils/kmeans.h
@@ -1,2 +1,16 @@
 #pragma once
 
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+#include "distance.h"
+#include "random_utils.h"
+
+namespace alaya {
+
+void kmeans(const float* kData, const std::size_t kDataNum, const std::size_t kDataDim,
+            std::vector<std::vector<float>>& centroids, unsigned& cluster_num,
+            const unsigned kKMeansIter);
+
+}  // namespace alaya
