a8a973ad070e187797a1319c7b6406bea10bff77
BenjaminXiang <xiangl3@mail.sustech.edu.cn>
2024-03-20 22:33:30 +0800

    Update gtest example

--- tests/hello_test.cpp
+++ tests/hello_test.cpp
@@ -1,9 +1,9 @@
 #include <gtest/gtest.h>
 
 // Demonstrate some basic assertions.
-TEST(HelloTest, Basic) {
+TEST(HelloTest, BasicAssertions) {
   // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
+  EXPECT_STRNE("hello", "hello");
   // Expect equality.
   EXPECT_EQ(7 * 6, 42);
 }
